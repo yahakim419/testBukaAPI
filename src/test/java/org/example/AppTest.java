f7669d13c7faf489faafeeb387f4d35cd9740328
yahakim419 <yahakim419@gmail.com>
2021-10-26 23:31:01 +0700

    Update post function

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,9 +1,16 @@
 package org.example;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-
+import static org.hamcrest.Matchers.*;
 import io.restassured.RestAssured;
+import io.restassured.internal.common.assertion.Assertion;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import netscape.javascript.JSObject;
+import org.json.simple.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.awt.*;
@@ -14,19 +21,42 @@ import java.awt.*;
 public class AppTest 
 {
 
-    String Endpoint = "https://jsonplaceholder.cypress.io/posts/";
-    String parameter = "1";
+    String Endpoint = "https://jsonplaceholder.cypress.io/posts";
+
     @Test
-    public void checkSchema() {
+    public void assureDataType() {
         RestAssured.given()
                 .when()
-                .get(Endpoint+parameter)
+                .get(Endpoint)
                 .then()
                 .log()
                 .ifValidationFails()
                 .assertThat()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("schema.json"));
+                .body(matchesJsonSchemaInClasspath("schema2.json"));
+    }
+
+
+
+    @Test
+    public  void postTest(){
+        JSONObject requestParams = new JSONObject();
+
+        requestParams.put("title", "Recomendation");
+        requestParams.put("body", "Motorcycle");
+        requestParams.put("userId", 12);
+
+       RestAssured.given()
+                .contentType("application/json")
+                .body(requestParams.toJSONString())
+                .post(Endpoint)
+                .then()
+                .statusCode(201)
+                .body("title",equalTo("Recomendation")
+                ,"body",equalTo("Motorcycle")
+                ,"userId",equalTo(12),
+                "id",equalTo(101));
+
     }
 
 
